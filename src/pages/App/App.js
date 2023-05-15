9ece9500252d4bb352751071dae71e3dd5f1ddbf
Marv0912 <marvin.fajardo.s@icloud.com>
2023-05-15 14:31:15 -0400

    utilities

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { getUser } from '../../utilities/users-service';
+import HomePage from '../HomePage/HomePage';
+import AuthPage from '../AuthPage/AuthPage';
+
 
 function App() {
-  return <div className="App">
+  const [user, setUser] = useState(getUser());
+  return (
 
-    
-  </div>;
+    <main className="App">
+      {user ? (
+        <>
+          {/* <NavBar user={user} setUser={setUser} /> */}
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
+
+
+    </main>
+
+
+  )
+
+
+
 }
 
 export default App;
