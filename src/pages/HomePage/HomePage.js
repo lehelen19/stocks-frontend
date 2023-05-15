ed78683c56b2ea883fdc0b1e9d5cb6c30ac82490
lehelen19 <le.helen19@gmail.com>
2023-05-15 15:11:42 -0700

    Add search, stockDetails state to home page and import searchbar

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import './HomePage.css';
+import SearchBar from '../../components/Sidebar/SearchBar/SearchBar';
 
 const HomePage = () => {
+  const [search, setSearch] = useState('');
+  const [stockDetails, setStockDetails] = useState(null);
+
   return (
     <div>
       HomePage
       <Sidebar />
+      <SearchBar />
     </div>
   );
 };
