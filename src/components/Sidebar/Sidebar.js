b2a73b8f5498e6752c3832e4902a37ea124a37fb
lehelen19 <le.helen19@gmail.com>
2023-05-16 11:55:23 -0700

    Create new watchlist input, button

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,6 +6,8 @@ import { getWatchlists } from '../../utilities/watchlists-service';
 
 function Sidebar({ user }) {
   const [watchlists, setWatchlists] = useState(null);
+  const [newWatchlist, setNewWatchlist] = useState(null);
+  const [showInput, setShowInput] = useState(false);
 
   const fetchWatchlists = async () => {
     try {
@@ -20,9 +22,21 @@ function Sidebar({ user }) {
     fetchWatchlists();
   }, []);
 
+  const handleClick = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <div className="user-sidebar">
       <h3>Welcome, {user.username}!</h3>
+
+      <button onClick={handleClick}>Create new watchlist</button>
+      {showInput && (
+        <form>
+          <input placeholder="New watchlist name..." />
+          <button>Submit</button>
+        </form>
+      )}
       <p>Watchlists: {watchlists}</p>
       {/* <CreateWatchlistButton onCreate={create} />  */}
       {/* <WatchlistList watchlists={watchlists} /> */}
