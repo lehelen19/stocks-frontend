550e5a504d6a354807cdbddc8869c1e03ef56b44
Marv0912 <marvin.fajardo.s@icloud.com>
2023-05-16 19:39:05 -0400

    75% functionality on updateWatchlistName

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,105 +1,105 @@
 import React, { useEffect, useState } from 'react';
 import {
-  getWatchlists,
-  createWatchlist,
-  deleteWatchlist,
-  updateWatchlistName
+    getWatchlists,
+    createWatchlist,
+    deleteWatchlist,
+    updateWatchlistName
 } from '../../utilities/watchlists-service';
 import { logOut } from '../../utilities/users-service';
 import { Link } from 'react-router-dom';
 
 function Sidebar({ user, setUser }) {
-  const [watchlists, setWatchlists] = useState(null);
-  const [watchlistName, setWatchlistName] = useState('');
-  const [showInput, setShowInput] = useState(false);
-  //   RENDER ERROR
-  const [error, setError] = useState('');
+    const [watchlists, setWatchlists] = useState(null);
+    const [watchlistName, setWatchlistName] = useState('');
+    const [showInput, setShowInput] = useState(false);
+    //   RENDER ERROR
+    const [error, setError] = useState('');
 
-  const fetchWatchlists = async () => {
-    try {
-      const foundWatchlists = await getWatchlists();
-      setWatchlists(foundWatchlists);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const fetchWatchlists = async () => {
+        try {
+            const foundWatchlists = await getWatchlists();
+            setWatchlists(foundWatchlists);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const handleLogout = () => {
-    logOut();
-    setUser(null);
-  };
+    const handleLogout = () => {
+        logOut();
+        setUser(null);
+    };
 
-  useEffect(() => {
-    fetchWatchlists();
-  }, []);
-  const handleClick = () => {
-    setShowInput(!showInput);
-  };
-
-  const handleChange = (e) => {
-    setWatchlistName(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await createWatchlist(watchlistName);
-      fetchWatchlists();
-    } catch (err) {
-      setError('New watchlist creation failed - try again');
-    }
-  };
-  const handleDeleteWatchlist = async (_id) => {
-    try {
-      await deleteWatchlist(_id);
-      fetchWatchlists();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleUpdateWatchlistName = async (id, newName) => {
-    try {
-        await updateWatchlistName(id, newName);
+    useEffect(() => {
         fetchWatchlists();
-    } catch (error) {
-        console.log(error);
-    }
-  }
-  return (
-    <div className="user-sidebar">
-      <h1>Welcome, {user.username}!</h1>
+    }, []);
+    const handleClick = () => {
+        setShowInput(!showInput);
+    };
 
-      <button onClick={handleClick}>Create new watchlist</button>
-      {showInput && (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={watchlistName}
-            onChange={handleChange}
-            placeholder="New watchlist name..."
-          />
-          <button>Submit</button>
-        </form>
-      )}
-      <section>
-        <h2>Watchlists</h2>
-        {watchlists &&
-          watchlists.map((watchlist) => {
-            const { name, _id } = watchlist;
-            return (
-              <div key={_id}>
-                <Link to={`/watchlists/${_id}`}>
-                  <p>{name}</p>
-                </Link>
-                <button onClick={() => handleDeleteWatchlist(_id)}>X</button>
-              </div>
-            );
-          })}
-      </section>
-      <button onClick={handleLogout}>Log Out</button>
-    </div>
-  );
+    const handleChange = (e) => {
+        setWatchlistName(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await createWatchlist(watchlistName);
+            fetchWatchlists();
+        } catch (err) {
+            setError('New watchlist creation failed - try again');
+        }
+    };
+    const handleDeleteWatchlist = async (_id) => {
+        try {
+            await deleteWatchlist(_id);
+            fetchWatchlists();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleUpdateWatchlistName = async (id, newName) => {
+        try {
+            await updateWatchlistName(id, newName);
+            fetchWatchlists();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className="user-sidebar">
+            <h1>Welcome, {user.username}!</h1>
+
+            <button onClick={handleClick}>Create new watchlist</button>
+            {showInput && (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={watchlistName}
+                        onChange={handleChange}
+                        placeholder="New watchlist name..."
+                    />
+                    <button>Submit</button>
+                </form>
+            )}
+            <section>
+                <h2>Watchlists</h2>
+                {watchlists &&
+                    watchlists.map((watchlist) => {
+                        const { name, _id } = watchlist;
+                        return (
+                            <div key={_id}>
+                                <Link to={`/watchlists/${_id}`}>
+                                    <p>{name}</p>
+                                </Link>
+                                <button onClick={() => handleDeleteWatchlist(_id)}>X</button>
+                            </div>
+                        );
+                    })}
+            </section>
+            <button onClick={handleLogout}>Log Out</button>
+        </div>
+    );
 }
 
 export default Sidebar;
--- src/utilities/watchlists-api.js
+++ src/utilities/watchlists-api.js
@@ -3,21 +3,21 @@ import sendRequest from './send-request';
 const BASE_URL = '/api/watchlists';
 
 export function getWatchlists() {
-  return sendRequest(BASE_URL);
+    return sendRequest(BASE_URL);
 }
 
 export function createWatchlist(watchlistName) {
-  return sendRequest(BASE_URL, 'POST', { name: watchlistName });
+    return sendRequest(BASE_URL, 'POST', { name: watchlistName });
 }
 
 export function deleteWatchlist(id) {
-  return sendRequest(`${BASE_URL}/${id}`, 'DELETE');
+    return sendRequest(`${BASE_URL}/${id}`, 'DELETE');
 }
 
 export function getWatchlistDetails(id) {
-  return sendRequest(`${BASE_URL}/${id}`);
+    return sendRequest(`${BASE_URL}/${id}`);
 }
 
 export function updateWatchlistName(id, newName) {
-  return sendRequest(`${BASE_URL}/${id}`, 'POST', { name: newName });
+    return sendRequest(`${BASE_URL}/${id}`, 'POST', { name: newName });
 }
