fbb0c42124f5ec50386b989d5830972ceea2df18
lehelen19 <le.helen19@gmail.com>
2023-05-16 13:45:40 -0700

    Write watchlistdetailpage fn

--- src/pages/WatchlistDetailPage/WatchlistDetailPage.js
+++ src/pages/WatchlistDetailPage/WatchlistDetailPage.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 const WatchlistDetailPage = ({ user, search, setSearch, handleSubmit }) => {
+  const [watchlistDetails, setWatchlistDetails] = useState(null);
+  const [error, setError] = useState('');
+
+  const { id } = useParams();
+
+  const fetchWatchlistDetails = async () => {
+    try {
+      const foundWatchlist = await getWatchlistDetail(id);
+      setWatchlistDetails(foundWatchlist);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <>
       <h3>Watchlist Details</h3>
