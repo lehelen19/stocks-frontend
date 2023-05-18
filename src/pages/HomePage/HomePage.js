591aa9edc5b37600a2c6df4281be3f17a572b2ae
Marv0912 <marvin.fajardo.s@icloud.com>
2023-05-18 16:21:43 -0400

    add images

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,18 +2,18 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 const HomePage = ({ user, setUser }) => {
-  return (
-    <div className="flex h-screen">
-      <div className="h-full">
-        <Sidebar user={user} setUser={setUser} />
-      </div>
-      <section className="flex-1 bg-teal-50">
-        <h1>Home Page</h1>
+    return (
+        <div className="flex h-screen">
+            <div className="h-full">
+                <Sidebar user={user} setUser={setUser} />
+            </div>
+            <section className="flex-1 bg-teal-50">
+                <h1>Home Page</h1>
 
-        <SearchBar />
-      </section>
-    </div>
-  );
+                <SearchBar />
+            </section>
+        </div>
+    );
 };
 
 export default HomePage;
