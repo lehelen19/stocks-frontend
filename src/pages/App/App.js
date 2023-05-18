4c55bacaf8c6b204d390166b4bd84a6f636482c0
lehelen19 <le.helen19@gmail.com>
2023-05-18 11:30:01 -0700

    Update/delete css files

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,6 @@
-import './App.css';
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import { useNavigate } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import AuthPage from '../AuthPage/AuthPage';
 import StockDetailPage from '../StockDetailPage/StockDetailPage';
@@ -10,16 +8,6 @@ import WatchlistDetailPage from '../WatchlistDetailPage/WatchlistDetailPage';
 
 function App() {
   const [user, setUser] = useState(getUser());
-  const [search, setSearch] = useState('');
-
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearch('');
-    navigate(`/stocks/${search}`);
-  };
-
   return (
     <main className="App">
       {user ? (
@@ -27,39 +15,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={
-                <HomePage
-                  user={user}
-                  setUser={setUser}
-                  search={search}
-                  setSearch={setSearch}
-                  handleSubmit={handleSubmit}
-                />
-              }
+              element={<HomePage user={user} setUser={setUser} />}
             />
             <Route
               path="/stocks/:symbol"
-              element={
-                <StockDetailPage
-                  user={user}
-                  setUser={setUser}
-                  search={search}
-                  setSearch={setSearch}
-                  handleSubmit={handleSubmit}
-                />
-              }
+              element={<StockDetailPage user={user} setUser={setUser} />}
             />
             <Route
               path="/watchlists/:id"
-              element={
-                <WatchlistDetailPage
-                  user={user}
-                  setUser={setUser}
-                  search={search}
-                  setSearch={setSearch}
-                  handleSubmit={handleSubmit}
-                />
-              }
+              element={<WatchlistDetailPage user={user} setUser={setUser} />}
             />
           </Routes>
         </>
