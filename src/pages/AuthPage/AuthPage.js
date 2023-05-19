dc05a341a1e6612e2bc711a6fd18885773a3a796
lehelen19 <le.helen19@gmail.com>
2023-05-18 23:55:39 -0700

    Delete authpage css

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -2,8 +2,6 @@ import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import { useState } from 'react';
 
-import './AuthPage.css';
-
 export default function AuthPage({ setUser }) {
   const [userStatus, setUserStatus] = useState(true);
   return (
@@ -12,12 +10,14 @@ export default function AuthPage({ setUser }) {
         {userStatus ? (
           <>
             <div className="left">
-              <div className='image-container w-16 h-16' >
-              <img 
-              className='object-contain w-full h-full ml-9 mt-2 rounded-md'
-              src={process.env.PUBLIC_URL + '/img/pink.avif'} alt="" />
+              <div className="image-container w-16 h-16">
+                <img
+                  className="object-contain w-full h-full ml-9 mt-2 rounded-md"
+                  src={process.env.PUBLIC_URL + '/img/pink.avif'}
+                  alt=""
+                />
               </div>
-              
+
               <button
                 className="hover:underline italic"
                 onClick={() => setUserStatus(!userStatus)}
@@ -25,16 +25,18 @@ export default function AuthPage({ setUser }) {
                 Want to create an account?
               </button>
             </div>
-            
+
             <LoginForm setUser={setUser} />
           </>
         ) : (
           <>
             <div className="left">
-            <div className='image-container w-16 h-16' >
-              <img 
-              className='object-contain w-full h-full ml-9 mt-2 rounded-md'
-              src={process.env.PUBLIC_URL + '/img/dab.avif'} alt="" />
+              <div className="image-container w-16 h-16">
+                <img
+                  className="object-contain w-full h-full ml-9 mt-2 rounded-md"
+                  src={process.env.PUBLIC_URL + '/img/dab.avif'}
+                  alt=""
+                />
               </div>
               <button
                 className="hover:underline italic"
